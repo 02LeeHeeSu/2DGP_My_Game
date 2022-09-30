5f23ad109881bf5fd17f40e5479bf82191bc7475
Hee SU Lee <lee46911375@gmail.com>
2022-09-30 23:47:19 +0900

    링크의 움직임을 담당할 코드의 기본 마련

---
+++ Link.py
@@ -0,0 +1,27 @@
+from pico2d import *
+
+running = True
+width, height = 1080, 960
+
+dir_x, dir_y = 0, 0
+pos_x, pos_y = width // 2, height // 2
+
+# run
+
+
+# roll
+
+
+# attack
+
+
+open_canvas(width, height)
+
+
+while running:
+    clear_canvas()
+    update_canvas()
+    delay(0.05)
+
+
+close_canvas()
