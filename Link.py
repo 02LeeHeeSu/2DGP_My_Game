ecd475103d414e89a88afefaa897c17f587fdc66
Hee SU Lee <lee46911375@gmail.com>
2022-10-02 04:03:16 +0900

    함수 복원

--- Link.py
+++ Link.py
@@ -9,8 +9,39 @@ dir_x, dir_y = 0, 0
 pos_x, pos_y = width // 2, height // 2
 
 # run
+def run_link():
+    global running
+    global direction
     global dir_x, dir_y
 
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+            elif event.key == SDLK_w:
+                direction = 0
+                dir_y += 1
+            elif event.key == SDLK_s:
+                direction = 1
+                dir_y -= 1
+            elif event.key == SDLK_d:
+                direction = 2
+                dir_x += 1
+            elif event.key == SDLK_a:
+                direction = 3
+                dir_x -= 1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_w:
+                dir_y -= 1
+            elif event.key == SDLK_s:
+                dir_y += 1
+            elif event.key == SDLK_d:
+                dir_x -= 1
+            elif event.key == SDLK_a:
+                dir_x += 1
 # roll
 
 
