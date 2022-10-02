a028acd4a9016e7e0633155f5fad23a9f1956172
Hee SU Lee <lee46911375@gmail.com>
2022-10-02 15:35:33 +0900

    l 키로 구르기 함수 동작

--- Link.py
+++ Link.py
@@ -93,9 +93,13 @@ while running:
                 running = False
             elif event.key == SDLK_w or event.key == SDLK_s or event.key == SDLK_d or event.key == SDLK_a:
                 run_kd(event.key)
+            elif event.key == SDLK_l:
+                roll_kd(event.key)
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_w or event.key == SDLK_s or event.key == SDLK_d or event.key == SDLK_a:
                 run_ku(event.key)
+            elif event.key == SDLK_l:
+                roll_ku(event.key)
 
     if (direction == 0 or direction == 1) and Run:
         run_y.clip_draw(run_y_Frame_w * 90, run_y_Frame_h * 120, 90, 120, pos_x, pos_y)
