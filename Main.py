e27b55ff1b418447a96fc52dd6cfffb93d477f10
Hee SU Lee <lee46911375@gmail.com>
2022-10-06 21:11:02 +0900

    Create Main.py
    기존의 Link.py 파일을 객체 지향 방식으로 작성하기 위한 코드 생성

---
+++ Main.py
@@ -0,0 +1,56 @@
+from pico2d import *
+
+running = True
+width, height = 1440, 960
+dir_x, dir_y = 0, 0
+
+
+def run_kd(key):
+    global dir_x, dir_y
+
+    if key == SDLK_w:
+        dir_y += 1
+    elif key == SDLK_s:
+        dir_y -= 1
+    elif key == SDLK_d:
+        dir_x += 1
+    elif key == SDLK_a:
+        dir_x -= 1
+
+
+def run_ku(key):
+    global dir_x, dir_y
+
+    if key == SDLK_w:
+        dir_y -= 1
+    elif key == SDLK_s:
+        dir_y += 1
+    elif key == SDLK_d:
+        dir_x -= 1
+    elif key == SDLK_a:
+        dir_x += 1
+
+
+# 주인공 객체 생성
+# 적 객체 생성
+
+
+# 초기화
+open_canvas(width, height)
+# 게임 루프
+while running:
+    clear_canvas()
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+    update_canvas()
+
+
+    delay(0.05)
+
+close_canvas()
