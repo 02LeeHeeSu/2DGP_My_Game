ec4ed032861c448f22634a9442de6d51002f910e
Hee SU Lee <lee46911375@gmail.com>
2022-11-04 22:31:16 +0900

    stand 상태 클래스로 정의

--- Link.py
+++ Link.py
@@ -17,6 +17,22 @@ key_event_table = {
     (SDL_KEYUP, SDLK_d): du,
     (SDL_KEYUP, SDLK_a): au
 }
+
+
+# 클래스를 이용해서 상태를 만든다
+class Stand:
+    @staticmethod
+    def enter(self, event):
+    @staticmethod
+    def exit(self):
+        pass
+    @staticmethod
+    def do(self):
+        pass
+
+    @staticmethod
+    def draw(self):
+        self.Stand_image.clip_draw(self.direction * 90, 0, 90, 120, self.x, self.y)
 class MainCharacter:
     def __init__(self):
         self.x, self.y = width // 2, height // 2   # 위치
