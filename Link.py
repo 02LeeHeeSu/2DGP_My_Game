6bf0f9e5edffe83fdb7be824b7711f95442709ab
Hee SU Lee <lee46911375@gmail.com>
2022-11-05 00:50:30 +0900

    인자를 넘겨주는 실수를...수정

--- Link.py
+++ Link.py
@@ -76,7 +76,7 @@ class RUN:
     @staticmethod
     def do(self):
         # 방향 설정
-        if self.dir_x == 0 and self.dir_y == 0 and self.is_none_event(self):
+        if self.dir_x == 0 and self.dir_y == 0 and self.is_none_event():
             self.add_event(dir_0)
         if self.dir_y > 0:
             self.direction = 0
