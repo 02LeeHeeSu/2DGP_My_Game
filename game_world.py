e63e7f2ef9d7d6cd2499e32b44253007998242b5
Hee SU Lee <lee46911375@gmail.com>
2022-11-30 14:38:21 +0900

    game_world에 충돌 처리 코드 추가

--- game_world.py
+++ game_world.py
@@ -1,6 +1,7 @@
 # game world
 
 world = [[], [], []]
+collision_group = dict()
 
 
 def add_object(o, depth):
@@ -11,11 +12,12 @@ def add_objects(o, depth):
     world[depth] += o
 
 
-def remove_object(o):
-    for layer in world:
-        if o in layer:
-            layer.remove(o)
-            del o
+def remove_object(o, depth):
+    for obj in world[depth]:
+        if obj == o:
+            world[depth].remove(obj)
+            remove_collision_object(obj)
+            del obj
             return
     raise ValueError('Trying destroy non existing object.')
 
@@ -31,3 +33,35 @@ def clear():
         del o
     for layer in world:
         layer.clear()
+
+
+def add_collision_group(a, b, group):
+    if group not in collision_group:
+        collision_group[group] = [[], []]
+
+    if a:
+        if type(a) is list:
+            collision_group[group][0] += a
+        else:
+            collision_group[group][0].append(a)
+
+    if b:
+        if type(b) is list:
+            collision_group[group][1] += b
+        else:
+            collision_group[group][1].append(b)
+
+
+def all_collision_pairs():
+    for group, pairs in collision_group.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+                yield a, b, group
+
+
+def remove_collision_object(o):
+    for pairs in collision_group.values():
+        if o in pairs[0]:
+            pairs[0].remove(o)
+        if o in pairs[1]:
+            pairs[1].remove(o)
