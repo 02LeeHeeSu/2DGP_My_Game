1e2c3b31deb21681e775b0057ef20dd27adc4dc6
Hee SU Lee <lee46911375@gmail.com>
2022-11-30 14:36:58 +0900

    로고 상태에서 종료 코드 추가

--- logo_state.py
+++ logo_state.py
@@ -40,6 +40,9 @@ def draw():
 
 def handle_events():
     events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
 
 
 def test_self():
