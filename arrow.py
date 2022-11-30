1e99a1eec8f6f332891a82df33c2d8d934205e24
Hee SU Lee <lee46911375@gmail.com>
2022-11-30 14:48:42 +0900

    방향 정의에 따른 수정

--- arrow.py
+++ arrow.py
@@ -19,26 +19,26 @@ class Arrow:
         if self.d == 0:
             self.image.clip_draw(0, 0, 25, 75, self.x, self.y)
 
-        elif self.d == 1:
+        elif self.d == defined_direction['down']:
             self.image.clip_composite_draw(0, 0, 25, 75, 0, 'v', self.x, self.y, 25, 75)
 
-        if self.d == 2:
+        if self.d == defined_direction['right']:
             self.image.clip_composite_draw(0, 0, 25, 75, -3.141592 / 2.0, '', self.x, self.y, 25, 75)
 
-        elif self.d == 3:
+        elif self.d == defined_direction['left']:
             self.image.clip_composite_draw(0, 0, 25, 75, 3.141592 / 2.0, '', self.x, self.y, 25, 75)
 
     def update(self):
         if self.ot < 0.4:
-            game_world.remove_object(self)
+            game_world.remove_object(self, level['Arrow'])
 
-        if self.d == 0:
+        if self.d == defined_direction['up']:
             self.y += self.velocity * game_framework.frame_time
 
-        elif self.d == 1:
+        elif self.d == defined_direction['down']:
             self.y -= self.velocity * game_framework.frame_time
 
-        if self.d == 2:
+        if self.d == defined_direction['right']:
             self.x += self.velocity * game_framework.frame_time
 
         elif self.d == 3:
