55d355382aa43c17298c1bb71572d32333dddc3d
Hee SU Lee <lee46911375@gmail.com>
2022-11-30 14:43:57 +0900

    Pixel_Per_Meter와 객체 이동 속도 변수 정의

---
+++ define_PPM.py
@@ -0,0 +1,16 @@
+Pixel_Per_Meter = (10.0 / 0.15)
+
+KM_Per_Hour_link = 20.0
+Meter_Per_Minute_link = (KM_Per_Hour_link * 1000.0 / 60.0)
+Meter_Per_Sec_link = (Meter_Per_Minute_link / 60.0)
+Pixel_Per_Sec_link = (Meter_Per_Sec_link * Pixel_Per_Meter)
+
+KM_Per_Hour_chu = 10.0
+Meter_Per_Minute_chu = (KM_Per_Hour_chu * 1000.0 / 60.0)
+Meter_Per_Sec_chu = (Meter_Per_Minute_chu / 60.0)
+Pixel_Per_Sec_chu = (Meter_Per_Sec_chu * Pixel_Per_Meter)
+
+KM_Per_Hour_octo = 5.0
+Meter_Per_Minute_octo = (KM_Per_Hour_octo * 1000.0 / 60.0)
+Meter_Per_Sec_octo = (Meter_Per_Minute_octo / 60.0)
+Pixel_Per_Sec_octo = (Meter_Per_Sec_octo * Pixel_Per_Meter)
