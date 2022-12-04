3629a90b053ed2311eef6bb31f231f6a536870ff
Hee SU Lee <lee46911375@gmail.com>
2022-12-05 00:33:10 +0900

    배경 클래스 생성자 수정

--- background.py
+++ background.py
@@ -13,10 +13,9 @@ from door import Door
 class Background:
     def __init__(self):
         self.image = [[load_image('Background/background_%d%d.png' % (x, y)) for y in range(3)] for x in range(3)]
-        self.x_index, self.y_index = x_index['center'], y_index['top']
-        self.w = self.image[self.x_index][self.y_index].w
-        self.h = self.image[self.x_index][self.y_index].h
-        self.cur_room = room_info[self.x_index][self.y_index]
+        self.x, self.y = stage_info.cur_room[0], stage_info.cur_room[1]
+        self.w = self.image[self.x][self.y].w
+        self.h = self.image[self.x][self.y].h
 
     def draw(self):
         self.image[self.x_index][self.y_index].clip_draw_to_origin(self.window_left, self.window_bottom,
