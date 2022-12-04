c7b1a7cf22bdd454639f8cafd94d79a1fd796121
Hee SU Lee <lee46911375@gmail.com>
2022-12-05 00:35:43 +0900

    Update background.py
    1. 모듈 import
    2. 스테이지 클리어 후 Door 객체 추가하는 함수 open_door 구현
    3. 배경 객체가 방 정보에 따라 몬스터를 추가하고 문을 열도록 update 수정

--- background.py
+++ background.py
@@ -1,13 +1,27 @@
 from pico2d import *
 
+import game_world
 from canvas_size import width, height
 
 import server
 
-from door import Door
+import stage_info
+from depth import level
+
+import door
+from item import Item
 
 
+def open_door():
+    if item.Item_Queue:
+        dropped_item = Item()
+        game_world.add_object(dropped_item, level['Objects'])
+        game_world.add_collision_group(None, dropped_item, 'Link:Item')
 
+    for d in door.door_info[stage_info.cur_room[0]][stage_info.cur_room[1]]:
+        door_obj = door.Door(d)
+        game_world.add_object(door_obj, level['Door'])
+        game_world.add_collision_group(None, door_obj, 'Link:Door')
 
 
 class Background:
@@ -25,6 +39,13 @@ class Background:
     def update(self):
         self.window_left = clamp(0, int(server.link.x) - width // 2, self.w - width - 1)
         self.window_bottom = clamp(0, int(server.link.y) - height // 2, self.h - height - 1)
+
+        if stage_info.monsters_info[self.x][self.y]:
+            stage_info.add_monsters()
+
+        if not game_world.world[level['Monsters']] and not door.is_opened_door:
+            door.is_opened_door = True
+            open_door()
 
     def handle_event(self, event):
         pass
