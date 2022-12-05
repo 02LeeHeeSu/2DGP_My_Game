b2c699633a878b639ebbfbeccfda86d941a26255
Hee SU Lee <lee46911375@gmail.com>
2022-12-06 02:42:26 +0900

    Update rock.py
    1. 함수 순서 수정
    2. 객체 제거 시 검사 추가

--- rock.py
+++ rock.py
@@ -10,10 +10,43 @@ from depth import level
 Time_Per_Attack = 1.0
 Attack_Per_Time = 1.0 / Time_Per_Attack
 FPAttack = 3
-
+1
 
 class Rock:
     image = None
+
+    def __init__(self, x, y, velocity, direction, distance):
+        if Rock.image is None:
+            Rock.image = load_image('Monsters/Octorok/rock.png')
+        self.x, self.y, self.velocity, self.direction, self.distance = x, y, velocity, direction, distance
+        self.init_x, self.init_y = x, y
+        self.frame = 0
+
+    def update(self):
+        self.frame = (self.frame + FPAttack * Attack_Per_Time * game_framework.frame_time) % FPAttack
+
+        if self.direction == up:
+            self.y += self.velocity * game_framework.frame_time
+
+        elif self.direction == down:
+            self.y -= self.velocity * game_framework.frame_time
+
+        if self.direction == right:
+            self.x += self.velocity * game_framework.frame_time
+
+        elif self.direction == left:
+            self.x -= self.velocity * game_framework.frame_time
+
+        if self.x < self.init_x - self.distance or self.x > self.init_x + self.distance:
+            game_world.remove_object(self, level['Objects'])
+
+        if self.y < self.init_y - self.distance or self.y > self.init_y + self.distance:
+            game_world.remove_object(self, level['Objects'])
+
+    def draw(self):
+        sx, sy = self.x - server.bg.window_left, self.y - server.bg.window_bottom
+        self.image.clip_draw(int(self.frame) * 40, 0, 40, 40, sx, sy)
+        draw_rectangle(*self.get_bb())
 
     def handle_event(self, event):
         pass
@@ -42,40 +75,9 @@ class Rock:
                 self.direction = right
 
         if group == 'Rock:ChuChu':
-            game_world.remove_object(self, level['Objects'])
+            if self in game_world.world[level['Objects']]:
+                game_world.remove_object(self, level['Objects'])
 
         if group == 'Rock:Octorok':
-            game_world.remove_object(self, level['Objects'])
-
-    def __init__(self, x, y, velocity, direction, distance):
-        if Rock.image is None:
-            Rock.image = load_image('Monsters/Octorok/rock.png')
-        self.x, self.y, self.velocity, self.direction, self.distance = x, y, velocity, direction, distance
-        self.init_x, self.init_y = x, y
-        self.frame = 0
-
-    def draw(self):
-        sx, sy = self.x - server.bg.window_left, self.y - server.bg.window_bottom
-        self.image.clip_draw(int(self.frame) * 40, 0, 40, 40, sx, sy)
-        draw_rectangle(*self.get_bb())
-
-    def update(self):
-        self.frame = (self.frame + FPAttack * Attack_Per_Time * game_framework.frame_time) % FPAttack
-
-        if self.direction == up:
-            self.y += self.velocity * game_framework.frame_time
-
-        elif self.direction == down:
-            self.y -= self.velocity * game_framework.frame_time
-
-        if self.direction == right:
-            self.x += self.velocity * game_framework.frame_time
-
-        elif self.direction == left:
-            self.x -= self.velocity * game_framework.frame_time
-
-        if self.x < self.init_x - self.distance or self.x > self.init_x + self.distance:
-            game_world.remove_object(self, level['Objects'])
-
-        if self.y < self.init_y - self.distance or self.y > self.init_y + self.distance:
-            game_world.remove_object(self, level['Objects'])
+            if self in game_world.world[level['Objects']]:
+                game_world.remove_object(self, level['Objects'])
