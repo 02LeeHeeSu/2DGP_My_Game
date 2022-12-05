3989e7c04e91a070e2a69f0123050971d18cc011
Hee SU Lee <lee46911375@gmail.com>
2022-12-06 02:31:00 +0900

    Update arrow.py
    1. 함수 순서 일관성 있게 수정
    2. 사운드 출력
    3. 객체 제거 시 검사 추가

--- arrow.py
+++ arrow.py
@@ -10,44 +10,17 @@ from depth import level
 
 class Arrow:
     image = None
-
-    def handle_event(self, event):
-        pass
-
-    def get_bb(self):
-        sx, sy = self.x - server.bg.window_left, self.y - server.bg.window_bottom
-
-        if self.d == up or self.d == down:
-            return sx - 12.5, sy - 37.5, sx + 12.5, sy + 37.5
-
-        elif self.d == right or self.d == left:
-            return sx - 37.5, sy - 12.5, sx + 37.5, sy + 12.5
-
-    def handle_collision(self, other, group):
-        if self in game_world.world[level['Objects']]:
-            game_world.remove_object(self, level['Objects'])
+    shoot_sound = None
 
     def __init__(self, x, y, velocity, d, ot):
         if Arrow.image is None:
             Arrow.image = load_image('Link/Item/arrow.png')
         self.x, self.y, self.velocity, self.d, self.ot = x, y, velocity, d, ot
         self.init_x, self.init_y = x, y
-
-    def draw(self):
-        sx, sy = self.x - server.bg.window_left, self.y - server.bg.window_bottom
-
-        draw_rectangle(*self.get_bb())
-        if self.d == up:
-            self.image.clip_draw(0, 0, 25, 75, sx, sy)
-
-        elif self.d == down:
-            self.image.clip_composite_draw(0, 0, 25, 75, 0, 'v', sx, sy, 25, 75)
-
-        if self.d == right:
-            self.image.clip_composite_draw(0, 0, 25, 75, -3.141592 / 2.0, '', sx, sy, 25, 75)
-
-        elif self.d == left:
-            self.image.clip_composite_draw(0, 0, 25, 75, 3.141592 / 2.0, '', sx, sy, 25, 75)
+        if Arrow.shoot_sound is None:
+            Arrow.shoot_sound = load_wav('Sound/Objects/Arrow_Shoot.wav')
+        if self.ot >= 0.4:
+            Arrow.shoot_sound.play()
 
     def update(self):
         if self.ot < 0.4:
@@ -69,4 +42,36 @@ class Arrow:
             game_world.remove_object(self, level['Objects'])
 
         if self.y < self.init_y - (self.velocity * self.ot) or self.y > self.init_y + (self.velocity * self.ot):
+            game_world.remove_object(self, level['Objects'])
+
+    def draw(self):
+        sx, sy = self.x - server.bg.window_left, self.y - server.bg.window_bottom
+
+        draw_rectangle(*self.get_bb())
+        if self.d == up:
+            self.image.clip_draw(0, 0, 25, 75, sx, sy)
+
+        elif self.d == down:
+            self.image.clip_composite_draw(0, 0, 25, 75, 0, 'v', sx, sy, 25, 75)
+
+        if self.d == right:
+            self.image.clip_composite_draw(0, 0, 25, 75, -3.141592 / 2.0, '', sx, sy, 25, 75)
+
+        elif self.d == left:
+            self.image.clip_composite_draw(0, 0, 25, 75, 3.141592 / 2.0, '', sx, sy, 25, 75)
+
+    def handle_event(self, event):
+        pass
+
+    def get_bb(self):
+        sx, sy = self.x - server.bg.window_left, self.y - server.bg.window_bottom
+
+        if self.d == up or self.d == down:
+            return sx - 12.5, sy - 37.5, sx + 12.5, sy + 37.5
+
+        elif self.d == right or self.d == left:
+            return sx - 37.5, sy - 12.5, sx + 37.5, sy + 12.5
+
+    def handle_collision(self, other, group):
+        if self in game_world.world[level['Objects']]:
             game_world.remove_object(self, level['Objects'])
