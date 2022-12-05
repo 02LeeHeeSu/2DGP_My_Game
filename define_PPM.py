58eb271b7b3f279856a59e0584611f419d48f681
Hee SU Lee <lee46911375@gmail.com>
2022-12-06 02:33:25 +0900

    보스 몬스터와 sphere PPM 정의

--- define_PPM.py
+++ define_PPM.py
@@ -14,3 +14,13 @@ KM_Per_Hour_octo = 5.0
 Meter_Per_Minute_octo = (KM_Per_Hour_octo * 1000.0 / 60.0)
 Meter_Per_Sec_octo = (Meter_Per_Minute_octo / 60.0)
 Pixel_Per_Sec_octo = (Meter_Per_Sec_octo * Pixel_Per_Meter)
+
+KM_Per_Hour_vaati = 15.0
+Meter_Per_Minute_vaati = (KM_Per_Hour_vaati * 1000.0 / 60.0)
+Meter_Per_Sec_vaati = (Meter_Per_Minute_vaati / 60.0)
+Pixel_Per_Sec_vaati = (Meter_Per_Sec_vaati * Pixel_Per_Meter)
+
+KM_Per_Hour_sphere = 30.0
+Meter_Per_Minute_sphere = (KM_Per_Hour_sphere * 1000.0 / 60.0)
+Meter_Per_Sec_sphere = (Meter_Per_Minute_sphere / 60.0)
+Pixel_Per_Sec_sphere = (Meter_Per_Sec_sphere * Pixel_Per_Meter)
