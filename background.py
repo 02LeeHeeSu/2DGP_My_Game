71135ac03bcb333de13161e618212ecff4f266f3
Hee SU Lee <lee46911375@gmail.com>
2022-12-06 02:32:13 +0900

    Update background.py
    1. 함수 순서 수정
    2. 배경 음악 및 wav 파일 추가
    3. 보스 방 입장 시 배경 음악 교체

--- background.py
+++ background.py
@@ -1,5 +1,6 @@
 from pico2d import *
 
+import game_framework
 import game_world
 from canvas_size import width, height
 
@@ -30,13 +31,19 @@ class Background:
         self.x, self.y = stage_info.cur_room[0], stage_info.cur_room[1]
         self.w = self.image[self.x][self.y].w
         self.h = self.image[self.x][self.y].h
-
-    def draw(self):
-        self.image[self.x][self.y].clip_draw_to_origin(self.window_left, self.window_bottom,
-                                                       width, height,
-                                                       0, 0)
+        self.is_in_boss_room = False
+        self.low_health_timer = 1.0
+        self.Musics = [load_music('Sound/Background/Main_BGM.mp3'),
+                       load_music('Sound/Background/Boss_BGM.mp3'),
+                       load_wav('Sound/Background/LowHealth.wav')]
+        for i in range(2):
+            self.Musics[i].set_volume(100)
+        self.Musics[2].set_volume(72)
+        self.Musics[0].repeat_play()
 
     def update(self):
+        self.low_health_timer -= game_framework.frame_time
+
         self.window_left = clamp(0, int(server.link.x) - width // 2, self.w - width - 1)
         self.window_bottom = clamp(0, int(server.link.y) - height // 2, self.h - height - 1)
 
@@ -46,6 +53,19 @@ class Background:
         if not game_world.world[level['Monsters']] and not door.is_opened_door:
             door.is_opened_door = True
             open_door()
+
+        if stage_info.cur_room == [stage_info.center, stage_info.center] and not self.is_in_boss_room:
+            self.is_in_boss_room = True
+            self.Musics[0].stop()
+            self.Musics[1].repeat_play()
+
+        if server.link.current <= server.link.maximum / 3 and self.low_health_timer <= 0.0:
+            self.low_health_timer = 1.0
+            self.Musics[2].play()
+    def draw(self):
+        self.image[self.x][self.y].clip_draw_to_origin(self.window_left, self.window_bottom,
+                                                       width, height,
+                                                       0, 0)
 
     def handle_event(self, event):
         pass
