25684b4b1dc31aadce6e1eff1f8e048298460567
Hee SU Lee <lee46911375@gmail.com>
2022-12-06 02:40:08 +0900

    Update sword.py
    1. 함수 순서 수정
    2. 충돌 처리 수정
    3. 충돌 범위 수정

--- sword.py
+++ sword.py
@@ -1,31 +1,15 @@
 from pico2d import *
+
 import game_world
 import server
+
+from vaati import Vaati
 
 from define_dir import up, down, right, left
 from depth import level
 
 
 class Sword:
-    def get_bb(self):
-        sx, sy = self.x - server.bg.window_left, self.y - server.bg.window_bottom
-        
-        if server.link.Attack:
-            if self.direction == up:
-                return sx - 75, sy, sx + 75, sy + 100
-            elif self.direction == down:
-                return sx - 75, sy - 100, sx + 75, sy
-            elif self.direction == right:
-                return sx, sy - 75, sx + 100, sy + 75
-            elif self.direction == left:
-                return sx - 100, sy - 75, sx, sy + 75
-
-        if server.link.Spin:
-            return sx - 150, sy - 127.5 - 25, sx + 150, sy + 127.5 - 25
-
-        else:
-            return 0, 0, 0, 0
-
     def __init__(self, direction):
         self.x, self.y = server.link.x, server.link.y
         self.direction = direction
@@ -39,6 +23,25 @@ class Sword:
     def handle_event(self, event):
         pass
 
+    def get_bb(self):
+        sx, sy = self.x - server.bg.window_left, self.y - server.bg.window_bottom
+
+        if server.link.Attack:
+            if self.direction == up:
+                return sx - 75, sy, sx + 75, sy + 150
+            elif self.direction == down:
+                return sx - 75, sy - 150, sx + 75, sy
+            elif self.direction == right:
+                return sx, sy - 75, sx + 150, sy + 75
+            elif self.direction == left:
+                return sx - 150, sy - 75, sx, sy + 75
+
+        if server.link.Spin:
+            return sx - 150, sy - 127.5 - 25, sx + 150, sy + 127.5 - 25
+
     def handle_collision(self, other, group):
-        pass
+        if group == 'Sword:Monster':
+            if type(other) is Vaati and other.is_stopped:
+                if self in game_world.world[level['Objects']]:
+                    game_world.remove_object(self, level['Objects'])
 
