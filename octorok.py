3180ad1e3dd2d1c2182834f259aec2747f27807b
Hee SU Lee <lee46911375@gmail.com>
2022-12-06 02:41:51 +0900

    Update octorok.py
    1. 미사용 모듈 제거
    2. 공격 속도 빠르게
    3. 함수 순서 수정
    4. 사운드 추가
    5. 몬스터 범위 수정
    6. AI 일부 수정

--- octorok.py
+++ octorok.py
@@ -14,15 +14,13 @@ from rock import Rock
 
 from BehaviorTree import BehaviorTree, Leaf
 
-from canvas_size import width, height
-
-PPS_rock = 4.0 * Pixel_Per_Sec_octo
+PPS_rock = 6.0 * Pixel_Per_Sec_octo
 
 Time_Per_Move = 1.0
 Move_Per_Time = 1.0 / Time_Per_Move
 FPMove = 2
 
-Time_Per_Attack = 1.0
+Time_Per_Attack = 0.5
 Attack_Per_Time = 1.0 / Time_Per_Attack
 FPAttack = 2
 
@@ -43,22 +41,12 @@ animation_names = ['move', 'attack']
 
 class Octorok:
     images = None
-
-    def load_images(self):
-        if Octorok.images is None:
-            Octorok.images = {}
-            for name in animation_names:
-                Octorok.images[name] = [load_image("Monsters/Octorok/" + name + "%d" % i + "-" + "%d" % j + ".png") for
-                                        i, j in [(0, 1), (0, 2), (1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2)]]
-
-    def get_bb(self):
-        sx, sy = self.x - server.bg.window_left, self.y - server.bg.window_bottom
-
-        return sx - 40, sy - 45, sx + 40, sy + 45
+    hit_sound = None
 
     def __init__(self):
-        self.x = random.randint(40 + 192 + 45 + 1, server.bg.w - 40 - 192 - 45 - 1)
-        self.y = random.randint(45 + 192 + 60 + 1, server.bg.h - 45 - 192 - 60 - 1)
+        self.x = random.randint(40 + 192 * 2 + 45 + 1, server.bg.w - 40 - 192 * 2 - 45 - 1)
+        self.y = random.randint(45 + 192 * 2 + 60 + 1, server.bg.h - 45 - 192 * 2 - 60 - 1)
+        self.tx, self.ty = self.x, self.y
         self.load_images()
         self.dir = random.randint(up, left)
         self.speed = 0
@@ -69,144 +57,8 @@ class Octorok:
         self.frame_move = 0
         self.frame_attack = 0
         self.build_behavior_tree()
-
-    def look_player(self):
-        dx = server.link.x - self.x
-        dy = server.link.y - self.y
-
-        if dx >= 0 and dy >= 0:
-            if dx >= dy:
-                self.dir = right
-            else:
-                self.dir = up
-
-        if dx >= 0 > dy:
-            dy = absolute(dy)
-
-            if dx >= dy:
-                self.dir = right
-            else:
-                self.dir = down
-
-        if dx < 0 <= dy:
-            dx = absolute(dx)
-
-            if dx >= dy:
-                self.dir = left
-            else:
-                self.dir = up
-
-        if dx < 0 and dy < 0:
-            dx = absolute(dx)
-            dy = absolute(dy)
-
-            if dx >= dy:
-                self.dir = left
-            else:
-                self.dir = down
-
-    def look_random(self):
-        dx = random.randint(40, width - 40) - self.x
-        dy = random.randint(45, height - 45) - self.y
-
-        if dx >= 0 and dy >= 0:
-            if dx >= dy:
-                self.dir = right
-            else:
-                self.dir = up
-
-        if dx >= 0 > dy:
-            dy = absolute(dy)
-
-            if dx >= dy:
-                self.dir = right
-            else:
-                self.dir = down
-
-        if dx < 0 <= dy:
-            dx = absolute(dx)
-
-            if dx >= dy:
-                self.dir = left
-            else:
-                self.dir = up
-
-        if dx < 0 and dy < 0:
-            dx = absolute(dx)
-            dy = absolute(dy)
-
-            if dx >= dy:
-                self.dir = left
-            else:
-                self.dir = down
-
-    def check_attack_distance_is_in_range(self):
-        distance = calculate_distance(server.link, self)
-
-        if distance > self.attack_distance:
-            if self.wander_timer <= 0.0:
-                self.look_random()
-            self.speed = Pixel_Per_Sec_octo
-            self.Attack = False
-
-        self.look_player()
-
-        if distance <= self.attack_distance:
-            if self.dir == up or self.dir == down:
-                if self.x - 45 <= server.link.x <= self.x + 45:
-                    self.speed = 0
-                    self.Attack = True
-                    return BehaviorTree.SUCCESS
-                else:
-                    return BehaviorTree.FAIL
-
-            if self.dir == right or self.dir == left:
-                if self.y - 60 <= server.link.y <= self.y + 60:
-                    self.speed = 0
-                    self.Attack = True
-                    return BehaviorTree.SUCCESS
-                else:
-                    return BehaviorTree.FAIL
-
-        return BehaviorTree.FAIL
-
-    def build_behavior_tree(self):
-        check_node = Leaf('Check attack distance is in range', self.check_attack_distance_is_in_range)
-
-        self.bt = BehaviorTree(check_node)
-
-    def shoot_rock(self):
-        if self.dir == up:
-            threw_rock = Rock(self.x, self.y + 45 + 20 + 1, PPS_rock, self.dir, self.attack_distance)
-            game_world.add_object(threw_rock, level['Objects'])
-            game_world.add_collision_group(None, threw_rock, 'Link:Rock')
-            game_world.add_collision_group(threw_rock, None, 'Rock:Shield')
-            game_world.add_collision_group(threw_rock, None, 'Rock:ChuChu')
-            game_world.add_collision_group(threw_rock, None, 'Rock:Octorok')
-
-        elif self.dir == down:
-            threw_rock = Rock(self.x, self.y - 45 - 20 - 1, PPS_rock, self.dir, self.attack_distance)
-            game_world.add_object(threw_rock, level['Objects'])
-            game_world.add_collision_group(None, threw_rock, 'Link:Rock')
-            game_world.add_collision_group(threw_rock, None, 'Rock:Shield')
-            game_world.add_collision_group(threw_rock, None, 'Rock:ChuChu')
-            game_world.add_collision_group(threw_rock, None, 'Rock:Octorok')
-
-        elif self.dir == right:
-            threw_rock = Rock(self.x + 40 + 20 + 1, self.y, PPS_rock, self.dir, self.attack_distance)
-            game_world.add_object(threw_rock, level['Objects'])
-            game_world.add_collision_group(None, threw_rock, 'Link:Rock')
-            game_world.add_collision_group(threw_rock, None, 'Rock:Shield')
-            game_world.add_collision_group(threw_rock, None, 'Rock:ChuChu')
-            game_world.add_collision_group(threw_rock, None, 'Rock:Octorok')
-
-        elif self.dir == left:
-            threw_rock = Rock(self.x - 40 - 20 - 1, self.y, PPS_rock, self.dir, self.attack_distance)
-            game_world.add_object(threw_rock, level['Objects'])
-            game_world.add_collision_group(None, threw_rock, 'Link:Rock')
-            game_world.add_collision_group(threw_rock, None, 'Rock:Shield')
-            game_world.add_collision_group(threw_rock, None, 'Rock:ChuChu')
-            game_world.add_collision_group(threw_rock, None, 'Rock:Octorok')
+        if Octorok.hit_sound is None:
+            Octorok.hit_sound = load_wav('Sound/Enemy/Enemy_Hit.wav')
 
     def update(self):
         self.bt.run()
@@ -246,14 +98,30 @@ class Octorok:
             Octorok.images['move'][int(self.frame_move) + self.dir * 2].draw(sx, sy, 80, 90)
         draw_rectangle(*self.get_bb())
 
+    def load_images(self):
+        if Octorok.images is None:
+            Octorok.images = {}
+            for name in animation_names:
+                Octorok.images[name] = [load_image("Monsters/Octorok/" + name + "%d" % i + "-" + "%d" % j + ".png") for
+                                        i, j in [(0, 1), (0, 2), (1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2)]]
+
     def handle_event(self, event):
         pass
 
+    def get_bb(self):
+        sx, sy = self.x - server.bg.window_left, self.y - server.bg.window_bottom
+
+        return sx - 40, sy - 45, sx + 40, sy + 45
+
     def handle_collision(self, other, group):
         if group == 'Sword:Monster':
-            game_world.remove_object(self, level['Monsters'])
+            Octorok.hit_sound.play()
+            if self in game_world.world[level['Monsters']]:
+                game_world.remove_object(self, level['Monsters'])
         if group == 'Arrow:Monster':
-            game_world.remove_object(self, level['Monsters'])
+            Octorok.hit_sound.play()
+            if self in game_world.world[level['Monsters']]:
+                game_world.remove_object(self, level['Monsters'])
         if group == 'Shield:Monster':
             if self.dir == up:
                 self.y -= 100
@@ -268,4 +136,135 @@ class Octorok:
                 self.x += 100
                 self.x = clamp(192 + 45, self.x, server.bg.w - 192 - 45)
         if group == 'Rock:Octorok':
-            game_world.remove_object(self, level['Monsters'])
+            if self in game_world.world[level['Monsters']]:
+                game_world.remove_object(self, level['Monsters'])
+
+    def look_player(self):
+        dx = server.link.x - self.x
+        dy = server.link.y - self.y
+
+        if dx >= 0 and dy >= 0:
+            if dx >= dy:
+                self.dir = right
+            else:
+                self.dir = up
+
+        if dx >= 0 > dy:
+            dy = absolute(dy)
+
+            if dx >= dy:
+                self.dir = right
+            else:
+                self.dir = down
+
+        if dx < 0 <= dy:
+            dx = absolute(dx)
+
+            if dx >= dy:
+                self.dir = left
+            else:
+                self.dir = up
+
+        if dx < 0 and dy < 0:
+            dx = absolute(dx)
+            dy = absolute(dy)
+
+            if dx >= dy:
+                self.dir = left
+            else:
+                self.dir = down
+
+    def look_random(self):
+        self.tx = random.randint(40 + 192 * 2 + 45 + 1, server.bg.w - 40 - 192 * 2 - 45 - 1)
+        self.ty = random.randint(55 + 192 * 2 + 60 + 1, server.bg.h - 55 - 192 * 2 - 60 - 1)
+        dx = self.tx - self.x
+        dy = self.ty - self.y
+
+        if dx >= 0 and dy >= 0:
+            if dx >= dy:
+                self.dir = right
+            else:
+                self.dir = up
+
+        if dx >= 0 > dy:
+            dy = absolute(dy)
+
+            if dx >= dy:
+                self.dir = right
+            else:
+                self.dir = down
+
+        if dx < 0 <= dy:
+            dx = absolute(dx)
+
+            if dx >= dy:
+                self.dir = left
+            else:
+                self.dir = up
+
+        if dx < 0 and dy < 0:
+            dx = absolute(dx)
+            dy = absolute(dy)
+
+            if dx >= dy:
+                self.dir = left
+            else:
+                self.dir = down
+
+    def check_attack_distance_is_in_range(self):
+        distance = calculate_distance(server.link, self)
+
+        if distance > self.attack_distance:
+            if self.wander_timer <= 0.0:
+                self.wander_timer = 1.0
+                self.look_random()
+            self.speed = Pixel_Per_Sec_octo
+            self.Attack = False
+
+            return BehaviorTree.FAIL
+
+        self.look_player()
+
+        if distance <= self.attack_distance:
+            self.Attack = True
+            return BehaviorTree.SUCCESS
+
+        return BehaviorTree.FAIL
+
+    def shoot_rock(self):
+        if self.dir == up:
+            threw_rock = Rock(self.x, self.y + 45 + 20 + 1, PPS_rock, self.dir, self.attack_distance)
+            game_world.add_object(threw_rock, level['Objects'])
+            game_world.add_collision_group(None, threw_rock, 'Link:Rock')
+            game_world.add_collision_group(threw_rock, None, 'Rock:Shield')
+            game_world.add_collision_group(threw_rock, None, 'Rock:ChuChu')
+            game_world.add_collision_group(threw_rock, None, 'Rock:Octorok')
+
+        elif self.dir == down:
+            threw_rock = Rock(self.x, self.y - 45 - 20 - 1, PPS_rock, self.dir, self.attack_distance)
+            game_world.add_object(threw_rock, level['Objects'])
+            game_world.add_collision_group(None, threw_rock, 'Link:Rock')
+            game_world.add_collision_group(threw_rock, None, 'Rock:Shield')
+            game_world.add_collision_group(threw_rock, None, 'Rock:ChuChu')
+            game_world.add_collision_group(threw_rock, None, 'Rock:Octorok')
+
+        elif self.dir == right:
+            threw_rock = Rock(self.x + 40 + 20 + 1, self.y, PPS_rock, self.dir, self.attack_distance)
+            game_world.add_object(threw_rock, level['Objects'])
+            game_world.add_collision_group(None, threw_rock, 'Link:Rock')
+            game_world.add_collision_group(threw_rock, None, 'Rock:Shield')
+            game_world.add_collision_group(threw_rock, None, 'Rock:ChuChu')
+            game_world.add_collision_group(threw_rock, None, 'Rock:Octorok')
+
+        elif self.dir == left:
+            threw_rock = Rock(self.x - 40 - 20 - 1, self.y, PPS_rock, self.dir, self.attack_distance)
+            game_world.add_object(threw_rock, level['Objects'])
+            game_world.add_collision_group(None, threw_rock, 'Link:Rock')
+            game_world.add_collision_group(threw_rock, None, 'Rock:Shield')
+            game_world.add_collision_group(threw_rock, None, 'Rock:ChuChu')
+            game_world.add_collision_group(threw_rock, None, 'Rock:Octorok')
+
+    def build_behavior_tree(self):
+        check_node = Leaf('Check attack distance is in range', self.check_attack_distance_is_in_range)
+
+        self.bt = BehaviorTree(check_node)
